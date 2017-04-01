603a9beb5dab585cc9292d47e6fea291f3b620bc
rmskaff <ritamskaff@gmail.com>
2017-04-01 10:02:34 -0400

    Add files via upload

---
+++ Skaff.js
@@ -0,0 +1,5 @@
+function displayPhrase() {
+    var content = document.getElementById('comments');
+    var button = document.getElementById('change-phrase');
+    content.value= content.value + "Submitted successfully!";
+}
